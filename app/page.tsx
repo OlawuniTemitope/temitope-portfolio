d7bbbfbb0c9a5c36d8805485f6ad0b1342e9d805
Temitope Olawuni <olawunitemitope68@gmail.com>
2025-06-12 11:31:05 +0100

    removing ui

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Approach from "@/components/Approach";
 import Footer from "@/components/Footer";
@@ -20,7 +19,6 @@ const Home = () => {
         <Hero />
         <Grid />
         <RecentProjects />
-        {/* <Clients /> */}
         <Experience />
         <Approach />
         <Footer/>
